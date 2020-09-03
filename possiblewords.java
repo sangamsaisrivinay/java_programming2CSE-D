04ae6fea0df40128e798c93a6f735ff663ca35db
sangamsaisrivinayreddy <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-09-03 16:25:34 +0000

    Create possiblewords.java

---
+++ possiblewords.java
@@ -0,0 +1,27 @@
+/*
+
+Identify Possible Words
+Detective Sherlock Holmes while solving a case stumbled upon a letter which has many words whose one character was 
+missing i.e., one character in the word was replaced by an underscore. For eg. “Fi_er”. He also found a thin strips of paper 
+which had a group of words separated by semicolons, for eg, “Fever;filer;Filter;Fixer;fiber;tailor;offer”. He could figure out 
+that the word whose one character was missing was one of the possible words from the thin strips of paper. Detective 
+Holmes has approached you ( a trustworthy friend and a computer programmer) asking help in identifying the possible 
+words for each incomplete word.
+You are expected to write a method to identify the set of possible words. The method identifyPossibleWords takes two 
+strings as input. Input1 contains the incomplete word and input2 is the string containing a set of words separated by 
+semicolons.
+
+TEST CASES
+SAMPLE INPUT
+SAMPLE OUTPUT
+Fi_er
+Fever;filer;Filter;Fixer;fiber;fibre;tailor;offer
+FILER;FIXER;FIBER
+Wo_d
+World;Wood;Word;Would
+WOOD;WORD
+Ti_er
+Talker;tapper;timber;toner
+ERROR-009
+
+*/
