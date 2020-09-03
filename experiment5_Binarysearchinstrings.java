e48372cfe5783a6d9125b3ab21c24f6c6362c743
sangamsaisrivinayreddy <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-09-03 09:17:18 -0600

    Update experiment5_Binarysearchinstrings.java

--- experiment5_Binarysearchinstrings.java
+++ experiment5_Binarysearchinstrings.java
@@ -1,3 +1,17 @@
+/*
+
+Experiment 5 - Binary Search
+
+To search for a word in a dictionary, you don’t review all the words; you just check one word in the middle and thus narrow down the set of remaining words to check. The divide and conquer principle may be helpful to search for a given word in a dictionary.
+
+Consider a sorted a list of numbers to apply the principle.
+
+Implement java code to search for a given word in a dictionary. (Binary Search)
+*/
+
+//code
+
+
 import java.io.*;
 import java.util.Scanner;
 
