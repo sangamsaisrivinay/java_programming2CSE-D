bff8cb1121c7e89c6d4c99003c4851a12cf4c1e0
sangamsaisrivinayreddy <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-09-03 16:31:57 +0000

    Create palindromedigit.java

---
+++ palindromedigit.java
@@ -0,0 +1,28 @@
+/*
+FIND THE ONE DIGIT TO BE REMOVED TO FORM A
+PALINDROME
+Assume that a given number input1 can become a
+palindrome if only one of its digits are removed, i.e., only
+one digit in its number is out of place. Find the digit that
+needs to be removed from input1 to convert input1 into a palindrome
+FIND THE ONE DIGIT TO BE REMOVED TO FORM A
+PALINDROME
+Assume that a given number input1 can become a
+palindrome if only one of its digits are removed, i.e., only
+one digit in its number is out of place. Find the digit that
+needs to be removed from input1 to convert input1 into a
+
+TEST CASES
+SAMPLE INPUT
+SAMPLE OUTPUT
+12332
+1
+251532
+3
+10101 
+-1
+981894
+4
+
+
+*/
