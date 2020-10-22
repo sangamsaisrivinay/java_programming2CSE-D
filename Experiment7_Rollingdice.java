5ff98549d7b980cb2f67f8dabfde730440c7a9ee
sangamsaisrivinayreddy <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-10-22 08:50:13 -0600

    Create Experiment7_Rollingdice.java

---
+++ Experiment7_Rollingdice.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class Rollingdice{
+	public static void main(String args[]){
+		int c=0,d1,d2;
+		Random r=new Random();
+		for(int i=0;i<10000;i++){
+			d1=r.nextInt(6)+1;
+			d2=r.nextInt(6)+1;
+			if(d1==d2){
+				c+=1;
+			}
+		}
+		System.out.println(c);
+	}
+}
