fe69ad626e7b3f07a1d26fa31d55d70f730cc577
sangamsaisrivinayreddy <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-10-23 08:57:59 +0530

    Experiment10_gcd

---
+++ gcd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class gcd{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a and b values:");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int gcd=0;
+        int temp=n1;
+        int temp1=n2;
+        while (n1!=n2){
+            if(n1>n2)
+                n1-=n2;
+            else
+                n2-=n1;
+        }
+        gcd=n1;
+        System.out.printf("%d/%d=%d/%d",temp,temp1,temp/gcd,temp1/gcd);
+    }
+}
