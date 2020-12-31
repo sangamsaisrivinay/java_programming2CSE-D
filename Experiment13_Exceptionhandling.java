e5d4c0f865df9454c9fd3dab84f691f99988ef10
/Vinay <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-12-31 17:46:47 +0530

    Update and rename Experiment12_Exceptionhandling.java to Experiment13_Exceptionhandling.java

--- Experiment13_Exceptionhandling.java
+++ Experiment13_Exceptionhandling.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Experiment12_Exceptionhandling{
+public class Experiment13_Exceptionhandling{
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     int a,b;
