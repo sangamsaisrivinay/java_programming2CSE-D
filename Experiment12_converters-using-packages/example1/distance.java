80328f5a20538b06eb27f668249039db617133a5
/Vinay <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-12-31 17:59:35 +0530

    Create distance.java

---
+++ Experiment12_converters-using-packages/example1/distance.java
@@ -0,0 +1,9 @@
+package example1;
+public class distance{
+public static double km(double d){return d*1000;}
+public static double kc(double d){return d*100000;}
+public static double mk(double d){return d/1000;}
+public static double mc(double d){return d*100;}
+public static double ck(double d){return d/100000;}
+public static double cm(double d){return d/100;}
+}
