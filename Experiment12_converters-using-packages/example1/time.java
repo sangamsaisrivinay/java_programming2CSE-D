e90ee3de3784d7eb700bface4f47b0bcdb1ec180
/Vinay <68855472+sangamsaisrivinay@users.noreply.github.com>
2020-12-31 18:01:54 +0530

    Create time.java

---
+++ Experiment12_converters-using-packages/example1/time.java
@@ -0,0 +1,9 @@
+package example1;
+public class time{
+public static double hm(double t){return t*60;}
+public static double hs(double t){return t*3600;}
+public static double mh(double t){return t/60;}
+public static double ms(double t){return t*60;}
+public static double sm(double t){return t/60;}
+public static double sh(double t){return t/3600;}
+}
