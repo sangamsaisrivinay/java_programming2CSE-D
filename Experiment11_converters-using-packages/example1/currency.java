fab1c25d1b9a87b3b834c706d2515967b115f505
/Vinay <68855472+sangamsaisrivinay@users.noreply.github.com>
2021-01-04 15:18:39 +0530

    Delete currency.java

--- Experiment11_converters-using-packages/example1/currency.java
+++
@@ -1,9 +0,0 @@
-package example1;
-public class currency{
-public static double id(double a){return a*70;}
-public static double iy(double a){return a*0.71;}
-public static double ie(double a){return a*100;}
-public static double di(double a){return a/70;}
-public static double yi(double a){return a/0.71;}
-public static double ei(double a){return a/100;}
-}
