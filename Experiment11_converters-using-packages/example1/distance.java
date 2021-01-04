31da59048c79d813c42015d3d3f02ab27c6b26f6
/Vinay <68855472+sangamsaisrivinay@users.noreply.github.com>
2021-01-04 15:18:53 +0530

    Delete distance.java

--- Experiment11_converters-using-packages/example1/distance.java
+++
@@ -1,9 +0,0 @@
-package example1;
-public class distance{
-public static double km(double d){return d*1000;}
-public static double kc(double d){return d*100000;}
-public static double mk(double d){return d/1000;}
-public static double mc(double d){return d*100;}
-public static double ck(double d){return d/100000;}
-public static double cm(double d){return d/100;}
-}
